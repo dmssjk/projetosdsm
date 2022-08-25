de244f2e91dc46575d4d04825770d09665d00838
Daniel Machado <106353031+dmssjk@users.noreply.github.com>
2022-08-25 11:56:10 -0300

    Add files via upload
    gerador de lista automatico ainda nao esta pronto, trabalhando

---
+++ facilitadorlista.py
@@ -0,0 +1,10 @@
+e = int(input('quantos exercicios? '))
+a = str(input('nome do aluno? '))
+c = str(input('lista? '))
+print(c)
+print(f'Aluno {a}')
+
+for i in range(1,e+1):
+    print(f'               Exercicio {i}\n')
+
+
